8f3c1f2d8e96609771a370cc4ac9e1fff9656f08
Cody Brubaker and Ezra Sandzer-Bell <cody@>
2016-11-17 13:25:39 -0800

    add rspecs for multiple words in argument. also swaps out booleans for strings results

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,5 +1,26 @@
 class String
-  define_method(:anagram) do |variable|
+  define_method(:anagram?) do |testString|
+    result = ""
+    resultArray = []
+    objectArray = self.split("")
+    testwordArray = testString.split(" ")
 
+    testwordArray.each() do |testWord|
+      splitWordArray = testWord.split("")
+
+      if objectArray.length == splitWordArray.length
+        objectArray.each() do |letter|
+          if splitWordArray.include?(letter) == true
+            splitWordArray.delete(letter)
+          end
+        end
+      end
+
+      if splitWordArray == []
+        resultArray.push(testWord)
+      end
+    end
+    result = resultArray.join(" ")
+    result
   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,7 +3,16 @@ require('pry')
 require('anagrams.rb')
 
 describe('String#anagrams') do
-  it("what it does") do
-    expect("input".anagram("paramater")).to(eq("output"))
+  # it("tests for equivalence of method-object to argument") do
+  #   expect("fox".anagram?("fox")).to(eq(true))
+  # end
+  it("tests for non-anagram") do
+    expect("brake".anagram?("beak")).to(eq(""))
+  end
+  it("tests for anagram with correct length") do
+    expect("brake".anagram?("break")).to(eq("break"))
+  end
+  it("tests for anagram with one correct and one incorrect") do
+    expect("brake".anagram?("break pancake")).to(eq("break"))
   end
 end
